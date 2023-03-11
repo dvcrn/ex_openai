d34facb6638257470b3abd8be7e0626b05697b92
David Mohl <git@d.sh>
2023-03-11 14:00:42 +0900

    Add tests for image variation using files

---
+++ test/image_test.exs
@@ -0,0 +1,31 @@
+defmodule ExOpenAI.ImageTest do
+  use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  setup do
+    ExVCR.Config.cassette_library_dir("./test/fixture/vcr_cassettes")
+    :ok
+  end
+
+  test "image variation" do
+    use_cassette "image_variation" do
+      duck = File.read!("#{__DIR__}/testdata/duck.png")
+
+      {:ok, res} = ExOpenAI.Images.create_image_variation(duck)
+
+      assert Enum.count(res.data) == 1
+      assert List.first(res.data) |> Map.get(:url) |> is_binary()
+    end
+  end
+
+  test "image variation with parameters" do
+    use_cassette "image_variation_b64" do
+      duck = File.read!("#{__DIR__}/testdata/duck.png")
+
+      {:ok, res} = ExOpenAI.Images.create_image_variation(duck, response_format: "b64_json")
+
+      assert Enum.count(res.data) == 1
+      assert List.first(res.data) |> Map.get(:b64_json) |> is_binary()
+    end
+  end
+end
