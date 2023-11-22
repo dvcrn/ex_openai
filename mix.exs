f95f164166a9aff714b68148328f9253921bc0a1
David Mohl <git@d.sh>
2023-11-22 22:01:15 +0900

    Bump version to 1.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExOpenAI.MixProject do
   def project do
     [
       app: :ex_openai,
-      version: "1.3.1",
+      version: "1.4.0",
       elixir: "~> 1.11",
       description: description(),
       start_permanent: Mix.env() == :prod,
