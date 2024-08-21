6fedc821102c2fbce7489a8d4cd528fbbdf0801c
preciz <barna.kovacs@tutanota.com>
2024-08-21 16:35:41 +0200

    Remove comment

--- lib/ex_openai/config.ex
+++ lib/ex_openai/config.ex
@@ -33,9 +33,6 @@ defmodule ExOpenAI.Config do
   def api_key, do: get_config_value(:api_key)
   def org_key, do: get_config_value(:organization_key)
 
-  # API Url
-  # Other clients allow overriding via the OPENAI_API_URL/OPENAI_API_BASE environment variable,
-  # but this is set via config with the default being https://api.openai.com/v1
   def api_url(override \\ nil) do
     override || get_config_value(:base_url, @openai_url)
   end
